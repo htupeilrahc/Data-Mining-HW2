fcd6d59182a160aafaa0f5a40e064d9dfbbb3134
htupeilrahc <158792874+htupeilrahc@users.noreply.github.com>
2025-06-09 13:00:12 +0800

    Add files via upload

---
+++ scripts/anomaly_detection/Tube/Autoformer_Tube_al2.sh
@@ -0,0 +1,47 @@
+export CUDA_VISIBLE_DEVICES=0
+
+for i in 1 2 3 4 5
+do
+  python -u run.py \
+    --task_name anomaly_detection \
+    --is_training 1 \
+    --root_path ./dataset/Tube_atleasttwo/tube$i \
+    --model_id tube${i}Tube_atleasttwo \
+    --model Autoformer \
+    --data PSM \
+    --features M \
+    --seq_len 100 \
+    --pred_len 0 \
+    --d_model 64 \
+    --d_ff 64 \
+    --e_layers 2 \
+    --enc_in 15 \
+    --c_out 15 \
+    --top_k 3 \
+    --anomaly_ratio 1 \
+    --batch_size 128 \
+    --train_epochs 3
+done
+
+for i in 1 2 3 4 5
+do
+  python -u run.py \
+    --task_name anomaly_detection \
+    --is_training 1 \
+    --root_path ./dataset/Tube_atleasttwo_10f/tube$i \
+    --model_id tube${i}Tube_atleasttwo_10f \
+    --model Autoformer \
+    --data PSM \
+    --features M \
+    --seq_len 100 \
+    --pred_len 0 \
+    --d_model 64 \
+    --d_ff 64 \
+    --e_layers 2 \
+    --enc_in 10 \
+    --c_out 10 \
+    --top_k 3 \
+    --anomaly_ratio 1 \
+    --batch_size 128 \
+    --train_epochs 3
+done
